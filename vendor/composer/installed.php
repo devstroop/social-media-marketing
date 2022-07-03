3b5e80f4804d366c7c031b4765856b2931dcaa68
Akash Shaw <44998751+itsalfredakku@users.noreply.github.com>
2022-07-03 23:22:23 +0530

    Update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f6e22f0367980c9565f014cfc215eb0368c7645a',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f6e22f0367980c9565f014cfc215eb0368c7645a',
+            'dev_requirement' => false,
+        ),
+    ),
+);
